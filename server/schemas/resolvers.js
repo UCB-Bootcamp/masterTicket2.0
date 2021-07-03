357113a13bf30415234434badae56474f3861451
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-07-03 13:49:40 -0700

    post and user resolvers --need to be tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,17 @@ const resolvers = {
         posts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Post.find(params);
+        },
+        post: async (_, { _id }) => {
+            return Post.findOne( { _id });
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password');
+        },
+        user: async (_, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password');
         }
     }
 };
