ae0f511a0eef6310a2a82e618161a9f7d06a5495
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-07-03 13:14:23 -0700

    add typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { Post } = require('../models');
 
 const resolvers = {
     
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,12 @@
 const { gql } = requireI('apollo-server-express');
 
 const typeDefs = gql`
+    type Attend {
+        username: String!
+        attendingVar: Boolean!
+        postId: Post
+    }
+
     type Post {
         eventTitle: String!
         createdAt: String
@@ -13,18 +19,32 @@ const typeDefs = gql`
         featuredEvent: Boolean
         date: String!
         image: String!
-        attending: [Post]
+        attending: [Attend]
     }
 
     type User {
-
+        username: String!
+        email: String!
+        password: String!
+        posts: [Post]
+        attend: []
     }
 
     type Query {
-
+        posts: 
+        post:
+        users:
+        user:
     }
 
     type Mutation {
-
+        login:
+        addUser:
+        updateUser:
+        deleteUser:
+        addPost:
+        updatePost:
+        deletePost:
+        attend:
     }
 `;
