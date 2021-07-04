d151a20f14a084f72ddbc2c5015fa5d1ca35c8f1
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-07-03 18:06:11 -0700

    add authMiddleware to server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-//   context: authMiddleware
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
