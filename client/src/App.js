2ef8e4eeaa5e2e86349c18b3e6f3b8536ed1eb38
Leah Russell <leahsigridrussell@gmail.com>
2021-07-05 12:19:20 -0700

    Adding header elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import SingleThought from './pages/SinglePost';
+// import Dashboard from './pages/Dashboard';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          {/* <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/post/:id" component={SingleThought} />
+            </Switch>
+          </div>
+          <Footer /> */}
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
