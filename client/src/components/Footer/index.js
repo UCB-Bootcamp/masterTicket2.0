c30b343109011a9e8ba4dbedd32d20aa4d3cd4b9
Shygois <shayana.gois@att.net>
2021-07-05 18:09:27 -0700

    added footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+      <footer className="w-100 mt-auto bg-secondary p-4">
+        <div className="container">&copy;2020 by Shyyy</div>
+      </footer>
+    );
+  };
+
+export default Footer;
