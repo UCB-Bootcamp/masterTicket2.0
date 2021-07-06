02941ace3aa8dbb0fc4a6e79175c3e62ae55d1d8
TimWeyel <tweyel@gmail.com>
2021-07-06 13:22:42 -0700

    apollo is up and running! we have datagit add .git add .git add .

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PostList from '../components/PostList';
-// import { QUERY_POSTS } from '../utils/queries';
-// import { useQuery } from '@apollo/react-hooks';
+import { QUERY_POSTS } from '../utils/queries';
+import { useQuery } from '@apollo/react-hooks';
 
 
 const Home = () => {
-  // const { data } = useQuery(QUERY_POSTS);
-
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
+    console.log('posts', posts);
 
 
   return (
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,21 +1,15 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_POSTS = gql`
-  query posts($username: String) {
-    posts(username: $username) {
+  query {
+    posts {
       _id
-      eventTitle
-      createdAt
-      username
-      venue
-      city
-      band
-      genre
-      eventDescription
-      featuredEvent
       date
-      image
-      attending
+      band
+      eventTitle
+      username
+      city
+      venue
     }
   }
 `;
