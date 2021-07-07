e522fe0956d8ba3bddccfe561e8b2ae41e66b075
TimWeyel <tweyel@gmail.com>
2021-07-07 16:52:03 -0700

    post form rendering. data shown in console. WIP: handleSubmit for createPost

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -1,75 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { CREATE_POST } from '../../utils/mutations';
+
 const PostForm = () => {
-  // const [formState, setFormState] = useState('');
-  // const { name, email, message } = formState;
+  const [formState, setFormState] = useState({"eventTitle": '', "venue": '', "city": '', "band": '', "genre": '', "date": '', "event-description": ''});
+  // const { eventTitle, venue, city, band, genre, date } = formState;
   // const [errorMessage, setErrorMessage] = useState('');
-  // function handleChange(e) {
-  //   if (e.target.name === 'email') {
-  //     const isValid = validateEmail(e.target.value);
-  //     console.log(isValid);
-  //     if (!isValid) {
-  //       setErrorMessage('Your email is invalid')
-  //     } else {
-  //       if (!e.target.value.length) {
-  //         setErrorMessage(`${e.target.mame} if required.`);
-  //       } else {
-  //         setErrorMessage('');
-  //       }
-  //     }
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  }
+  //   if (!e.target.value.length) {
+  //     setErrorMessage(`${e.target.mame} if required.`);
+  //   } else {
+  //     setErrorMessage('');
   //   }
+  // }
   //   if (!errorMessage) {
   //     setFormState({...formState, [e.target.name]: e.target.value });
   //   }
-  // }
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   console.log(formState);
-  // }
+  
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const { data } = await createPost({
+        variables: { ...formState }
+      });
+
+      Post.createPost(data.createPost);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      emailLogin: '',
+      passwordLogin: '',
+			emailSignup: '',
+			passwordSignup: ''
+    });
+    console.log(formState);
+  }
+  
   return (
     <div className="col new-post-form card">
       <h5 className="main-title text-center card-header">Create an Event</h5>
       <div className="col card-body">
-        <form className="topBefore form">
+        <form className="topBefore form" onSubmit={handleSubmit}>
           <p>BLAHHHHH</p>
-            <input id="event-title" type="text" placeholder="EVENT" />
-            <input id="venue" type="text" placeholder="VENUE" />
-            <input id="city" type="text" placeholder="CITY" />
-            <input id="band" type="text" placeholder="BAND" />
-            <input id="genre" type="text" placeholder="GENRE" />
-            <input id="date" type="text" placeholder="DATE (ex. Oct 21, 2025)" />
+            <input onChange={handleChange} name="eventTitle" id="event-title" type="text" placeholder="EVENT" />
+            <input onChange={handleChange} name="venue" id="venue" type="text" placeholder="VENUE" />
+            <input onChange={handleChange} name="city" id="city" type="text" placeholder="CITY" />
+            <input onChange={handleChange} name="band" id="band" type="text" placeholder="BAND" />
+            <input onChange={handleChange} name="genre" id="genre" type="text" placeholder="GENRE" />
+            <input onChange={handleChange} name="date" id="date" type="text" placeholder="DATE (ex. Oct 21, 2025)" />
             <div className="featured-event container">
               <input name="featured-event" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
               <label for="featured-event">This is a Featured Event!</label>
             </div>
             <textarea id="event-description" type="text" placeholder="EVENT DESCRIPTION"></textarea>
-            <input id="submit" type="submit" value="GO!" />
+            <input onChange={handleChange} id="submit" type="submit" value="GO!" />
         </form>
       </div>
     </div>
     )
   }
-//   <section>
-//   <h1>Contact me</h1>
-//   <form id="contact-form" onSubmit={handleSubmit}>
-//     <div>
-//       <label htmlFor="name">Name:</label>
-//       <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
-//     </div>
-//     <div>
-//       <label htmlFor="email">Email address:</label>
-//       <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
-//     </div>
-//     <div>
-//       <label htmlFor="message">Message:</label>
-//       <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
-//     </div>
-//     {errorMessage && (
-//       <div>
-//         <p classNameName="error-text">{errorMessage}</p>
-//       </div>
-//     )}
-//     <button type="submit">Submit</button>
-//   </form>
-// </section>
+
 
 export default PostForm;
