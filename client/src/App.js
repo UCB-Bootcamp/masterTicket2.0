ed11c575c1b6c133a9a36bf4142d464eafebb263
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-07-06 20:36:49 -0700

    more css stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
+// import 'App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
