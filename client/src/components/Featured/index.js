f218d02a3cf30b2e9b24c11b4db497b218be781e
Shygois <shayana.gois@att.net>
2021-07-07 11:34:23 -0700

    cleaned up code to remove console errors

--- client/src/components/Featured/index.js
+++ client/src/components/Featured/index.js
@@ -6,22 +6,20 @@ const Featured = () => {
 
     const { data } = useQuery(QUERY_FEAT_POSTS);
     const featuredEvent = data?.featuredEvent || [];
-    console.log('featuredEvent', featuredEvent);
-    console.log('data', data);
 
   return (
-    <section class="container" id="featured">
-      <row class="header">
-        <h5 class="text-center">--featured--</h5>
+    <section className="container" id="featured">
+      <div className="header row">
+        <h5 className="text-center">--featured--</h5>
 
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
+        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
           {featuredEvent &&
             featuredEvent.map((post, i) => (
-              <div class="carousel-item">
-                <img src={post.image} class="d-block w-100" alt="..." />
+              <div className="carousel-item" key={i}>
+                <img src={post.image} className="d-block w-100" alt="..." />
 
-                <div class="carousel-caption d-none d-md-block">
+                <div className="carousel-caption d-none d-md-block">
 
                   <h3>FEATURED EVENT: {post.eventTitle}</h3>
                   <p>Wouldn't you want to join in??</p>
@@ -30,17 +28,17 @@ const Featured = () => {
             ))}
           </div>
 
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="bi bi-skip-backward" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="bi bi-skip-backward" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
           </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="bi bi-skip-forward" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="bi bi-skip-forward" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
           </a>
         </div>
 
-      </row>
+      </div>
     </section>
   );
 }
