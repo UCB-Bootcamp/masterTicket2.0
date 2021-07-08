4b86205cb2ad596e642be7b0db4c892f51eb88e6
TimWeyel <tweyel@gmail.com>
2021-07-08 15:41:18 -0700

    checkbox state is working. now, how do we set state to featured-event?

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -6,9 +6,35 @@ const PostForm = () => {
   // needed to create new featuredEvent variable for checkbox
   const [formState, setFormState] = useState({"eventTitle": '', "venue": '', "city": '', "band": '', "genre": '', "date": '', "eventDescription": '' });
   const [createPost, { error }] = useMutation(TEST_CREATE_POST);
-  const [checkBoxState, setCheckBoxState] = useState({"featuredEvent": true});
-  const [uncheckBoxState, setUncheckBoxState] = useState({"featuredEvent": false});
-  //const [checkBoxState, setCheckBoxState] = useState(true);
+
+  const [checkBoxState, setCheckBoxState] = useState(false);
+  // const [featuredEventState, setFeaturedEventState] = useState(false);
+  // console.log('initial checkBoxState', checkBoxState);
+  // console.log('initial featuredEventState', featuredEventState);
+
+  // const handleCheckboxClick = (e) => {
+  //   console.log('target b4 click', e.target);
+  //   console.log('value b4 click', e.target.value);
+  //   console.log('checked b4 click', e.target.checked);
+  //   console.log('name b4 click', e.target.name);
+  //   console.log('id b4 click', e.target.id);
+  //   console.log('type b4 click', e.target.type);
+  //   console.log('defaultChecked b4 click', e.target.defaultChecked);
+  //   console.log('checkBoxState b4 click', checkBoxState);
+  //   console.log('featuredEventState b4 click', featuredEventState);
+  //   setCheckBoxState(!checkBoxState);
+  //   setFeaturedEventState(!featuredEventState);
+  //   console.log('target after click', e.target);
+  //   console.log('value after click', e.target.value);
+  //   console.log('checked after click', e.target.checked);
+  //   console.log('name after click', e.target.name);
+  //   console.log('id after click', e.target.id);
+  //   console.log('type after click', e.target.type);
+  //   console.log('defaultChecked after click', e.target.defaultChecked);
+  //   console.log('checkBoxState after click', checkBoxState);
+  //   console.log('featuredEventState after click', featuredEventState);
+
+  // }
   //const state = { checked: false }
   
   //console.log('Initial checkBoxState',checkBoxState)
@@ -21,7 +47,9 @@ const PostForm = () => {
       [name]: value
       // checked: value
     });
-  }
+
+    setCheckBoxState(!checkBoxState);
+  };
   
   // class ExampleApp extends React.Component {
   //   state = { checked: false }
@@ -49,35 +77,30 @@ const PostForm = () => {
   //   })
   // };
 
-  const handleChangeCheckbox = (e) => {
-    const { name, value } = e.target;
-    //setState({ checked: e.target.checked })
-    if (e.target.checked) {
-      setCheckBoxState({
-        checkBoxState,
-        [name]: value
-      })
+  // const handleChangeCheckbox = (e) => {
+  //   const { name, value } = e.target;
+  //   //setState({ checked: e.target.checked })
+  //   if (e.target.checked) {
+  //     setCheckBoxState({
+  //       checkBoxState,
+  //       [name]: value
+  //     })
       
-      //formState.featuredEvent;
-      // setCheckBoxState({
-      //   [e.target.checked]: e.target.checked
-      // });
-    } else {
-      setUncheckBoxState({
-        uncheckBoxState,
-        [name]: value
-      })
-    }
-    console.log('target', e.target);
-    console.log('value', e.target.value);
-    console.log('checked', e.target.checked);
-    console.log('name', e.target.name);
-    console.log('id', e.target.id);
-    console.log('type', e.target.type);
-    console.log('defaultChecked', e.target.defaultChecked);
-    console.log('checkBoxState', checkBoxState);
-    console.log('uncheckBoxState', uncheckBoxState);
-  };
+  //     //formState.featuredEvent;
+  //     // setCheckBoxState({
+  //     //   [e.target.checked]: e.target.checked
+  //     // });
+  //   } 
+  //   console.log('target', e.target);
+  //   console.log('value', e.target.value);
+  //   console.log('checked', e.target.checked);
+  //   console.log('name', e.target.name);
+  //   console.log('id', e.target.id);
+  //   console.log('type', e.target.type);
+  //   console.log('defaultChecked', e.target.defaultChecked);
+  //   console.log('checkBoxState', checkBoxState);
+
+  // };
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -128,7 +151,8 @@ const PostForm = () => {
           <input onChange={handleChange} value={formState.genre} name="genre" id="genre" type="text" placeholder="GENRE" />
           <input onChange={handleChange} value={formState.date} name="date" id="date" type="text" placeholder="DATE (ex. Oct 21, 2025)" />
           <div className="featured-event container">
-            <input onChange={handleChangeCheckbox} value={checkBoxState.featuredEvent} name="featuredEvent" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
+            <input onChange={handleChange} label="checkbox" name="featuredEvent" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
+            <p>Is "Value 1" checked? {checkBoxState.toString()}</p>
             <label htmlFor="featured-event">This is a Featured Event!</label>
           </div>
           <textarea onChange={handleChange} name="eventDescription" id="event-description" type="text" placeholder="EVENT DESCRIPTION"></textarea>
