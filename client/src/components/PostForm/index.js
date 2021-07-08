d2b50d096b5ba8923e8f2f4c977416b5c53ffd18
TimWeyel <tweyel@gmail.com>
2021-07-08 12:08:56 -0700

    still working on featured event checkbox. progress though!

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -1,32 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, setState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_POST } from '../../utils/mutations';
 
 const PostForm = () => {
   // needed to create new featuredEvent variable for checkbox
-  const [formState, setFormState] = useState({"eventTitle": '', "venue": '', "city": '', "band": '', "genre": '', "date": '', "eventDescription": '', "featuredEvent": false});
+  const [formState, setFormState] = useState({"eventTitle": '', "venue": '', "city": '', "band": '', "genre": '', "date": '', "eventDescription": '' });
   const [createPost, { error }] = useMutation(CREATE_POST);
+  // const [checkBoxState, setCheckBoxState] = useState({"featuredEvent": false});
+  const [checkBoxState, setCheckBoxState] = useState(false);
+  console.log('Initial checkBoxState',checkBoxState)
 
   const handleChange = e => {
-    const { name, value, checked } = e.target;
-    // this.setState({
-    //   checked: checked
-    // })
-    // const checkValue = target.type ==='checkbox' ? target.checked : target.checkValue;
-
+    const { name, value } = e.target;
+    
     setFormState({
       ...formState,
-      [name]: value,
-      checked: value
+      [name]: value
+      // checked: value
     });
   }
   
   // const handleChangeCheckbox = (e) => {
-  //   console.log(e.target.checked);
-  //   setState({ 
-  //     isChecked: e.target.checked
-  //   });
-  // }
+  //   console.log("clicked");
+  //   const { name, value } = e.target;
+  //   setCheckedState({
+  //     [e.target.featuredEvent]: e.target.checked
+  //   })
+  // };
+
+  const handleChangeCheckbox = (e) => {
+
+    if (e.target.defaultChecked) {
+      setCheckBoxState({
+        [e.target.featuredEvent]: e.target.checked
+      });
+    }
+    console.log('value', e.target.value);
+    console.log('checked', e.target.checked);
+    console.log('name', e.target.name);
+    console.log('id', e.target.id);
+    console.log('type', e.target.type);
+    console.log('featuredEvent', e.target.featuredEvent);
+    console.log('checkBoxState', checkBoxState);
+  };
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -48,7 +64,7 @@ const PostForm = () => {
       genre: '',
       date: '',
       eventDescription: '',
-      featuredEvent: true
+      featuredEvent: ''
     });
     console.log(formState);
   }
@@ -65,7 +81,7 @@ const PostForm = () => {
           <input onChange={handleChange} value={formState.genre} name="genre" id="genre" type="text" placeholder="GENRE" />
           <input onChange={handleChange} value={formState.date} name="date" id="date" type="text" placeholder="DATE (ex. Oct 21, 2025)" />
           <div className="featured-event container">
-            <input onChange={handleChange} value={formState.featuredEvent} name="featuredEvent" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
+            <input onChange={handleChangeCheckbox} defaultChecked={checkBoxState} name="featuredEvent" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
             <label htmlFor="featured-event">This is a Featured Event!</label>
           </div>
           <textarea onChange={handleChange} name="eventDescription" id="event-description" type="text" placeholder="EVENT DESCRIPTION"></textarea>
