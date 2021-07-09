4be29539328b5fde822e12830087e978fd75a510
TimWeyel <tweyel@gmail.com>
2021-07-09 10:33:08 -0700

    added MyPosts container

---
+++ client/src/components/MyPosts/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyPosts = () => {
+
+  return (
+    <div className=" col new-post-form card">
+      <div>
+        <h5 className="main-title text-center card-header">My Posts</h5>
+      </div>
+    </div>
+  )
+}
+
+export default MyPosts;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PostForm from '../components/PostForm';
+import MyPosts from '../components/MyPosts';
+
 const Dashboard = () => {
   return (
-    <PostForm />
+    <div className="row">
+      <PostForm />
+      <MyPosts />
+    </div>
   )
 };
 export default Dashboard;
