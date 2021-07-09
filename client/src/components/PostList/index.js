6e3d6ce079a4db4f5de352b6ef20632b3bb0aa9f
Leah Russell <leahsigridrussell@gmail.com>
2021-07-08 17:35:17 -0700

    fixing ui stuff

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -9,30 +9,47 @@ const PostList = ({posts}) => {
     <section className="container">
       <div className="row">
         <h5 className="main-title text-center">--new events--</h5>
-        <div className="cards col-md-6 col-lg-3 ">
         {posts &&
           posts.map((post, i) => (
-            <article className="card" key={i}>
-              <div className="card-info-hover">
-                <i className="bi bi-heart"></i>
-                <div className="card-clock-info">
-                  <i className="bi bi-clock"></i>
-                  <span className="card-time">{ post.date }</span>
+          <div className="cards col-md-6 col-lg-3 ">
+              <article className="card col-md-12" key={i}>
+                <div className="card-info">
+                  <span className="card-category">{ post.band }</span>
+                  <h3 className="card-title">{ post.eventTitle }</h3>
                 </div>
-                <div className="goto">
-                  <button> <a href='/post/{ post._id }'>checkout post</a></button>
+                <div className="card-info-hover">
+                  <div className="card-clock-info">
+                    <span className="card-time">{ post.date }</span>
+                  </div>
+                  <div className="goto">
+                    <button> <a href="/post/123">checkout post</a></button>
+                  </div>
                 </div>
-              </div>
-              <div className="card-img-hover">
-                <img src={ post.image } alt={ post.eventTitle }/>
-              </div>
-              <div className="card-info">
-                <span className="card-category">{ post.band }</span>
-                <h3 className="card-title">{ post.eventTitle }</h3>
-              </div>
-            </article>
+                {/* <div className="card-img-hover">
+                  <img src={ post.image } alt={ post.eventTitle }/>
+                </div> */}
+              </article>
+              {/* <article className="card" key={i}>
+                <div className="card-info-hover">
+                  <i className="bi bi-heart"></i>
+                  <div className="card-clock-info">
+                    <i className="bi bi-clock"></i>
+                    <span className="card-time">{ post.date }</span>
+                  </div>
+                  <div className="goto">
+                    <button> <a href="/post/123">checkout post</a></button>
+                  </div>
+                </div>
+                <div className="card-img-hover">
+                  <img src={ post.image } alt={ post.eventTitle }/>
+                </div>
+                <div className="card-info">
+                  <span className="card-category">{ post.band }</span>
+                  <h3 className="card-title">{ post.eventTitle }</h3>
+                </div>
+              </article> */}
+          </div>
         ))}
-        </div>
       </div>
     </section>
   )
