b654bdd92004e7fc28cd94d8c7c9d30478aacb33
Leah Russell <leahsigridrussell@gmail.com>
2021-07-08 17:58:07 -0700

    Revert "Feature/featured event checkbox"

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -4,10 +4,14 @@ import { CREATE_POST, TEST_CREATE_POST } from '../../utils/mutations';
 
 const PostForm = () => {
   // needed to create new featuredEvent variable for checkbox
-  const [formState, setFormState] = useState({"eventTitle": '', "venue": '', "city": '', "band": '', "genre": '', "date": '', "eventDescription": ''});
+  const [formState, setFormState] = useState({"eventTitle": '', "venue": '', "city": '', "band": '', "genre": '', "date": '', "eventDescription": '' });
   const [createPost, { error }] = useMutation(CREATE_POST);
-
-  const [checkBoxState, setCheckBoxState] = useState(false);
+  const [checkBoxState, setCheckBoxState] = useState({"featuredEvent": true});
+  const [uncheckBoxState, setUncheckBoxState] = useState({"featuredEvent": false});
+  //const [checkBoxState, setCheckBoxState] = useState(true);
+  //const state = { checked: false }
+  
+  //console.log('Initial checkBoxState',checkBoxState)
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -15,25 +19,86 @@ const PostForm = () => {
     setFormState({
       ...formState,
       [name]: value
+      // checked: value
     });
-  };
-
-  const handleCheckBoxClick = () => {
-    setCheckBoxState(!checkBoxState);
-
-    console.log(!checkBoxState);
   }
+  
+  // class ExampleApp extends React.Component {
+  //   state = { checked: false }
+  //   handleCheckboxChange = event =>
+  //     this.setState({ checked: event.target.checked })
+  //   render() {
+  //     return (
+  //       <div>
+  //         <label>
+  //           <Checkbox
+  //             checked={this.state.checked}
+  //             onChange={this.handleCheckboxChange}
+  //           />
+  //           <span>Label Text</span>
+  //         </label>
+  //       </div>    
+  //     ) 
+  //   }
+  // }
+  // const handleChangeCheckbox = (e) => {
+  //   console.log("clicked");
+  //   const { name, value } = e.target;
+  //   setCheckedState({
+  //     [e.target.featuredEvent]: e.target.checked
+  //   })
+  // };
+
+  const handleChangeCheckbox = (e) => {
+    const { name, value } = e.target;
+    //setState({ checked: e.target.checked })
+    if (e.target.checked) {
+      setCheckBoxState({
+        checkBoxState,
+        [name]: value
+      })
+      
+      //formState.featuredEvent;
+      // setCheckBoxState({
+      //   [e.target.checked]: e.target.checked
+      // });
+    } else {
+      setUncheckBoxState({
+        uncheckBoxState,
+        [name]: value
+      })
+    }
+    console.log('target', e.target);
+    console.log('value', e.target.value);
+    console.log('checked', e.target.checked);
+    console.log('name', e.target.name);
+    console.log('id', e.target.id);
+    console.log('type', e.target.type);
+    console.log('defaultChecked', e.target.defaultChecked);
+    console.log('checkBoxState', checkBoxState);
+    console.log('uncheckBoxState', uncheckBoxState);
+  };
 
   const handleSubmit = async e => {
     e.preventDefault();
     try {
       await createPost({
         variables: { 
+          // "eventTitle": "Rage Against the Machine",
+          // "username": "squidbeaks",
+          // "venue": "Santa Barbara Bowl",
+          // "city": "Santa Barbara",
+          // "band": "Weezer",
+          // "genre": "Alternative Rock",
+          // "eventDescription": "Blah blah blah",
+          // "featuredEvent": true,
+          // "date": "Sep 21, 2021"
           ...formState,
-          "featuredEvent": checkBoxState,
-          "username": "tweyel"
+          "featuredEvent": true,
+          "username": "squidbeaks"
         }
       });
+
     } catch (e) {
       console.error(e);
     }
@@ -63,8 +128,7 @@ const PostForm = () => {
           <input onChange={handleChange} value={formState.genre} name="genre" id="genre" type="text" placeholder="GENRE" />
           <input onChange={handleChange} value={formState.date} name="date" id="date" type="text" placeholder="DATE (ex. Oct 21, 2025)" />
           <div className="featured-event container">
-            <input onClick={handleCheckBoxClick} label="checkbox" name="featuredEvent" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
-            {/* <p>Is "Value 1" checked? {checkBoxState.toString()}</p> */}
+            <input onChange={handleChangeCheckbox} value={checkBoxState.featuredEvent} name="featuredEvent" id="featured-event" type="checkbox" placeholder="FEATURED EVENT" />
             <label htmlFor="featured-event">This is a Featured Event!</label>
           </div>
           <textarea onChange={handleChange} name="eventDescription" id="event-description" type="text" placeholder="EVENT DESCRIPTION"></textarea>
@@ -74,9 +138,7 @@ const PostForm = () => {
       </div>
     </div>
     )
-
-  };
-
+  }
 
 
 export default PostForm;
