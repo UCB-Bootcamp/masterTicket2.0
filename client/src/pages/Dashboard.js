75dc1260e8e43e67d353f83a7ae54e0cee958eb1
Tim Weyel <tweyel@gmail.com>
2021-07-15 10:46:38 -0700

    Revert "Feature/fix featured"

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,42 +1,8 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { GET_ME } from '../utils/queries';
-import Auth from '../auth';
 import PostForm from '../components/PostForm';
-import AttendingList from '../components/AttendingList';
-import MyPosts from '../components/MyPosts';
-
 const Dashboard = () => {
-
-  const { data } = useQuery(GET_ME);
-  const meData = data?.me || {};
-  const myPosts = data?.me.posts;
-  const username = data?.me.username;
-  const loggedIn = Auth.loggedIn();
-
-  if(!loggedIn) {
-    window.location.assign('/login');
-  }
-
   return (
-    <>
-      <section className="row dashboard-posts">
-        <div className="col new-post-form">
-          <PostForm 
-            username={username}
-          />
-        </div>
-        <div className="new-post-form">
-        <MyPosts 
-          myPosts={myPosts} 
-        />
-        </div>
-      </section>
-      <section className="container">  
-      <h5 className="main-title text-center">--Your Plans--</h5>
-        <AttendingList events={meData.attending}/>
-      </section>
-    </>
+    <PostForm />
   )
 };
 export default Dashboard;
