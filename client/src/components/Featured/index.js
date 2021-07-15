62d734be56ff879ad63bec68aac8141be184906a
TimWeyel <tweyel@gmail.com>
2021-07-14 18:02:44 -0700

    couldn't figure out why we were rendering empty divs on featured event so switch to use Carousel from react-bootstrap

--- client/src/components/Featured/index.js
+++ client/src/components/Featured/index.js
@@ -2,45 +2,38 @@ import React from 'react';
 import { QUERY_FEAT_POSTS } from '../../utils/queries';
 import { useQuery } from '@apollo/react-hooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel' 
 
 const Featured = () => {
 
     const { data } = useQuery(QUERY_FEAT_POSTS);
     const featuredEvent = data?.featuredEvent || [];
+    // console.log('featuredEvent', featuredEvent);
+    // console.log('featuredEvent[0]', featuredEvent[0].image );
 
   return (
-    <section className="container" id="featured">
-      <div className="header row">
-        <h5 className="text-center">--featured--</h5>
+    <section>
+      <h5 className="text-center" id="featured">--featured events--</h5>
+      <Carousel id="" className="controls" data-ride="carousel">
+        {featuredEvent.map(post => (
+          <Carousel.Item key={post.id} className="">
+            <img
+              className="d-block w-100"
+              src={post.image}
+              alt="..."
+            />
+            <Carousel.Caption>
+              <div class=" d-none d-md-block">
 
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-          <div className="carousel-inner">
-          {featuredEvent &&
-            featuredEvent.map((post, i) => (
-              <div className="carousel-item" key={i}>
-                <img src={post.image} className="d-block w-100" alt="..." />
-
-                <div className="carousel-caption d-none d-md-block">
-
-                  <h3>FEATURED EVENT: {post.eventTitle}</h3>
-                  <p>Wouldn't you want to join in??</p>
-                </div>
-              </div>
-            ))}
-          </div>
-
-          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span className="bi bi-skip-backward" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span className="bi bi-skip-forward" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
-
-      </div>
+            <h3>FEATURED EVENT: {post.eventTitle}</h3>
+            <p>Wouldn't you want to join in??</p>
+            </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </section>
+
   );
 }
 
