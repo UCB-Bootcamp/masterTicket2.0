813d83df99ca794331366871f1f0256cef0f2607
TimWeyel <tweyel@gmail.com>
2021-07-16 11:24:35 -0700

    some improvement on responsiveness for small screens

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -8,18 +8,18 @@ const PostList = ({ posts }) => {
 console.log('posts', posts);
   return (
 
-    <CardGroup className="flex-wrap">
+    <CardGroup className="flex-wrap sm-600">
 
     {posts &&
       posts.map((post, i) => (
-        <Card className="mr-3" key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
+        <Card className="mr-3 sm-600" key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
           <Card.Body>
             <Card.Text className="text-center pt-3 font-weight-bold tx-tfm title">{post.band}</Card.Text>
             {/* <Card.Title>{post.eventTitle}</Card.Title> */}
             <Card.Text className="text-center pt-3">{post.date}</Card.Text>
           </Card.Body>
           <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
-          <Button variant="secondary" className="m-auto"> <a href='/post/{ post._id }'>checkout post</a></Button>
+          <Button variant="secondary" className="m-auto sm-600"> <a href='/post/{ post._id }'>checkout post</a></Button>
         </Card>
     ))}
 
