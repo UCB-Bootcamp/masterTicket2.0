ba1230c178c3e15ead1cfbfe0ffb7aa257c1de8f
TimWeyel <tweyel@gmail.com>
2021-07-16 11:38:31 -0700

    more media queries

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -8,20 +8,20 @@ const PostList = ({ posts }) => {
 console.log('posts', posts);
   return (
 
-    <CardGroup className="flex-wrap sm-600">
+    <CardGroup className="flex-wrap sm-600 md-768">
 
-    {posts &&
-      posts.map((post, i) => (
-        <Card className="mr-3 sm-600" key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
-          <Card.Body>
-            <Card.Text className="text-center pt-3 font-weight-bold tx-tfm title">{post.band}</Card.Text>
-            {/* <Card.Title>{post.eventTitle}</Card.Title> */}
-            <Card.Text className="text-center pt-3">{post.date}</Card.Text>
-          </Card.Body>
-          <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
-          <Button variant="secondary" className="m-auto sm-600"> <a href='/post/{ post._id }'>checkout post</a></Button>
-        </Card>
-    ))}
+      {posts &&
+        posts.map((post, i) => (
+          <Card className="mr-3 sm-600 md-768 " key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
+            <Card.Body className="lg-992">
+              <Card.Text className="text-center pt-3 font-weight-bold tx-tfm title">{post.band}</Card.Text>
+              {/* <Card.Title>{post.eventTitle}</Card.Title> */}
+              <Card.Text className="text-center pt-3">{post.date}</Card.Text>
+            </Card.Body>
+            <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
+            <Button variant="secondary" className="m-auto sm-600 md-768"> <a href='/post/{ post._id }'>checkout post</a></Button>
+          </Card>
+      ))}
 
     </CardGroup>  
   )
