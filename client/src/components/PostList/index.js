19daa5dfaeae80541f805ed8b374e34787d001ca
TimWeyel <tweyel@gmail.com>
2021-07-16 10:03:56 -0700

    postlist images staying the same height now

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -12,14 +12,14 @@ console.log('posts', posts);
 
     {posts &&
       posts.map((post, i) => (
-        <Card  key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
+        <Card key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
           <Card.Body>
-            <Card.Text className="text-center pt-3 pb-3">{post.band}</Card.Text>
+            <Card.Text className="text-center pt-3">{post.band}</Card.Text>
             {/* <Card.Title>{post.eventTitle}</Card.Title> */}
-            <Card.Text className="m-auto float-left">{post.date}</Card.Text>
+            <Card.Text className="text-center pt-3">{post.date}</Card.Text>
           </Card.Body>
-          <Card.Img img src={post.image} alt={post.eventTitle} />
-          <Button variant="dark" className="m-auto float-right"> <a href='/post/{ post._id }'>checkout post</a></Button>
+          <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
+          <Button variant="secondary" className="m-auto"> <a href='/post/{ post._id }'>checkout post</a></Button>
         </Card>
     ))}
 
