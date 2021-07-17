37c099325a95eac2e512f2bda7808b88b81ade48
Leah Russell <leahsigridrussell@gmail.com>
2021-07-16 17:06:32 -0700

    Removing version 1.0

--- Version 1.0/__tests__/helpers.test.js
+++
@@ -1,6 +0,0 @@
-const { pluralize_people } = require('../utils/helpers');
-
-test('pluralize_people() returns correct pluralization of "person"', () => {
-    expect(pluralize_people(1)).toBe('person');
-    expect(pluralize_people(0)).toBe('people');
-});
