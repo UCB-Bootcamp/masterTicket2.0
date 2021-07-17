742f252b0cfa10897c2a85db312ea5f541e1fe0b
Leah Russell <leahsigridrussell@gmail.com>
2021-07-16 17:02:06 -0700

    Fixing post list

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,40 +1,29 @@
 import React from 'react';
+import { CardGroup , Card, Button} from 'react-bootstrap';
 
-const PostList = ({posts}) => {
+const PostList = ({ posts }) => {
   if (!posts.length) {
     return <h3>No posts yet!</h3>
   }
-
+console.log('posts', posts);
   return (
-    <section className="container">
-      <div className="row">
-        <h5 className="main-title text-center">--new events--</h5>
-        <div className="cards col-md-6 col-lg-3 ">
-        {posts &&
-          posts.map((post, i) => (
-            <article className="card" key={i}>
-              <div className="card-info-hover">
-                <i className="bi bi-heart"></i>
-                <div className="card-clock-info">
-                  <i className="bi bi-clock"></i>
-                  <span className="card-time">{ post.date }</span>
-                </div>
-                <div className="goto">
-                  <button> <a href='/post/{ post._id }'>checkout post</a></button>
-                </div>
-              </div>
-              <div className="card-img-hover">
-                <img src={ post.image } alt={ post.eventTitle }/>
-              </div>
-              <div className="card-info">
-                <span className="card-category">{ post.band }</span>
-                <h3 className="card-title">{ post.eventTitle }</h3>
-              </div>
-            </article>
-        ))}
-        </div>
-      </div>
-    </section>
+
+    <CardGroup className="flex-wrap sm-600 md-768">
+
+      {posts &&
+        posts.map((post, i) => (
+          <Card className="mr-3 sm-600 md-768 " key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
+            <Card.Body className="lg-992">
+              <Card.Text className="text-center pt-3 font-weight-bold tx-tfm title">{post.band}</Card.Text>
+              {/* <Card.Title>{post.eventTitle}</Card.Title> */}
+              <Card.Text className="text-center pt-3">{post.date}</Card.Text>
+            </Card.Body>
+            <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
+            <Button variant="secondary" className="m-auto sm-600 md-768 xl-button"> <a href='/post/{ post._id }'>checkout post</a></Button>
+          </Card>
+      ))}
+
+    </CardGroup>  
   )
 }
 
