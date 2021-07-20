7691fd0b062b815db29afe0e3df1cc235fe937b6
TimWeyel <tweyel@gmail.com>
2021-07-20 14:41:39 -0700

    post list items are now flexy and wrap responsively

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CardGroup , Card, Button} from 'react-bootstrap';
+import {Card, Button, Row, Col, Container} from 'react-bootstrap';
 
 const PostList = ({ posts }) => {
   if (!posts.length) {
@@ -7,23 +7,41 @@ const PostList = ({ posts }) => {
   }
 
   return (
+    
+    // <CardGroup className=" sm-600 md-768">
 
-    <CardGroup className="flex-wrap sm-600 md-768">
+    //   {posts &&
+    //     posts.map((post, i) => (
+    //       <Card className="mr-3 sm-600 md-768 " key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
+    //         <Card.Body className="lg-992">
+    //           <Card.Text className="text-center pt-3 font-weight-bold tx-tfm title">{post.band}</Card.Text>
+    //           {/* <Card.Title>{post.eventTitle}</Card.Title> */}
+    //           <Card.Text className="text-center pt-3">{post.date}</Card.Text>
+    //         </Card.Body>
+    //         <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
+    //         <Button variant="secondary" className="m-auto sm-600 md-768 xl-button" onClick={()=> (window.location.assign(`/post/${post._id}`))}>checkout post</Button>
+    //       </Card>
+    //   ))}
 
-      {posts &&
-        posts.map((post, i) => (
-          <Card className="mr-3 sm-600 md-768 " key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
-            <Card.Body className="lg-992">
-              <Card.Text className="text-center pt-3 font-weight-bold tx-tfm title">{post.band}</Card.Text>
-              {/* <Card.Title>{post.eventTitle}</Card.Title> */}
-              <Card.Text className="text-center pt-3">{post.date}</Card.Text>
-            </Card.Body>
-            <Card.Img className="card-img-top" src={post.image} alt={post.eventTitle} />
-            <Button variant="secondary" className="m-auto sm-600 md-768 xl-button" onClick={()=> (window.location.assign(`/post/${post._id}`))}>checkout post</Button>
-          </Card>
-      ))}
-
-    </CardGroup>  
+    // </CardGroup>  
+    <Container className="container " >
+      <Row className="postListContainer">
+        {/* <Col className="" > */}
+          {posts &&
+            posts.map((post, i) => (
+              <Card className="col-6 col-md-4 justify-content-around m-auto postList" key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
+                <Card.Body className="">
+                  <Card.Text className="">{post.band}</Card.Text>
+                  {/* <Card.Title>{post.eventTitle}</Card.Title> */}
+                  <Card.Text className="">{post.date}</Card.Text>
+                </Card.Body>
+                <Card.Img className="" src={post.image} alt={post.eventTitle} />
+                <Button variant="secondary" className="" onClick={()=> (window.location.assign(`/post/${post._id}`))}>checkout post</Button>
+              </Card>
+          ))}
+          {/* </Col> */}
+      </Row>
+    </Container>  
   )
 }
 
