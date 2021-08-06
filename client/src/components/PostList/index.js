7baf53842d5f6510a597b418dc73db0771bad892
TimWeyel <tweyel@gmail.com>
2021-08-06 16:59:56 -0700

    added venue to post-list and aligned band, venue, and date properly

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -12,14 +12,15 @@ const PostList = ({ posts }) => {
       {posts &&
         posts.map((post, i) => (
           <article className="card col-6 col-md-4 m-auto pb-3" key={i} onClick={()=> (window.location.assign(`/post/${post._id}`))}>
-            <div className="card-body ">
-
-              <h5 className="card-title">{post.band}</h5>
-              {/* <h5 className="card-title">{post.venue}</h5> */}
-              <div>
-              <p className="card-text">{post.date}</p>
+            <div className="card-body band-venue-container">
+              <div className="band-venue">
+                <h5 className="card-title">{post.band}</h5>
+              </div>
+              <div className="band-venue">
+                <h5 className="card-title">{post.venue}</h5>
               </div>
             </div>
+            <p className="card-text date">{post.date}</p>
             <img className="card-img-top mb-3" src={post.image} alt={post.eventTitle} />
             <button type="button" variant="secondary" className="btn btn-light btn-sm checkout-post-button" onClick={()=> (window.location.assign(`/post/${post._id}`))}>checkout post</button>
           </article>
